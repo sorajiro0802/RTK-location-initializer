56f27025d878c153c5a7a6c2cd762fad550e3067
sorajiro0802 <sora_potato_0802@icloud.com>
2023-01-19 23:53:00 +0900

    replaced lines

--- locationUpdater.py
+++ locationUpdater.py
@@ -1,18 +1,23 @@
 import math
 
+def attention():
+    print(
+        """
+        ※ 区切りはピリオドで入力してください
+        入力例: 35.40.52.9746 (<= 35° 40′ 52.9746″)
+        """
+        )
+
 def makeBack(file_path):
     with open(file_path, mode='r') as f:
         lines = f.read()
     backup_name = f"backup_{file_path}"
     with open(backup_name, mode='w') as f:
         f.write(lines)
-def attention():
-    print(
-        "入力例: 35.40.52.9746 <== 35° 40′ 52.9746″\n"
-        )
     
 def main():
     pass
+    attention()
 
 if __name__=='__main__':
     main()
